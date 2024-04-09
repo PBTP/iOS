9231636f7738310cbae8b3bacd7d8f155948f2f5
bulmang <hmk113737@gmail.com>
2024-04-10 00:19:28 +0900

    [update]: update Dependencies

--- Mongle/Tuist/Dependencies.swift
+++ Mongle/Tuist/Dependencies.swift
@@ -8,23 +8,17 @@
 import ProjectDescription
 import ProjectDescriptionHelpers
 
-let dependecies = Dependencies(
+let spm = SwiftPackageManagerDependencies([
+    .remote(
+        url: "https://github.com/pointfreeco/swift-composable-architecture.git",
+        requirement: .upToNextMajor(from: "1.9.0")
+    )
+])
+
+let dependencies = Dependencies(
     carthage: nil,
-    swiftPackageManager: SwiftPackageManagerDependencies(
-        [
-            .remote(
-                url: "https://github.com/pointfreeco/swift-composable-architecture.git",
-                requirement: .upToNextMajor(from: "1.9.0")
-            )
-        ],
-        baseSettings: .settings(
-            configurations: [
-                .debug(name: "DEV"),
-                .debug(name: "RELEASE"),
-                .release(name: "MASTER")
-            ]
-        ),
-        targetSettings: [:]
-    ),
+    swiftPackageManager: spm,
     platforms: [.iOS]
 )
+
+
