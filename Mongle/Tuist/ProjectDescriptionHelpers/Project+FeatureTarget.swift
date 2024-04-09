bcf753ab42eae8843ccdf568e121dbf20ebe6cee
bulmang <hmk113737@gmail.com>
2024-04-10 00:16:24 +0900

    [add] create FeatureTarget

---
+++ Mongle/Tuist/ProjectDescriptionHelpers/Project+FeatureTarget.swift
@@ -0,0 +1,20 @@
+//
+//  Project+FeatureTarget.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by bulmang on 4/9/24.
+//
+
+import Foundation
+import ProjectDescription
+
+public enum FeatureTarget {
+    case app    // iOSApp
+    case frameWork    // iOSApp
+    case unitTest   // Unit Test
+    case uiTest // UI Test
+    case demo   // Feature Excutable Test
+}
+
+
+
