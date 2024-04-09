b430dea21f8e0001712e2495aed51d89987e342d
bulmang <hmk113737@gmail.com>
2024-04-10 00:16:05 +0900

    [add] create infoPlist

---
+++ Mongle/Tuist/ProjectDescriptionHelpers/Project+InfoPlist.swift
@@ -0,0 +1,36 @@
+//
+//  Project+InfoPlist.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by bulmang on 4/9/24.
+//
+
+import ProjectDescription
+
+public extension Project {
+    static let appInfoPlist: [String: Plist.Value] = [
+        "CFBundleShortVersionString": "\(Environment.version)",
+        "CFBundleVersion": "\(Environment.bundleVersion)",
+        "CFBundleDisplayName": "$(APP_DISPLAY_NAME)",
+        "CFBundleDevelopmentRegion": "ko",
+        "CFBundleIdentifier": "\(Environment.bundlePrefix).release",
+        "DEFINES_MODULE": "NO",
+        "UISupportedInterfaceOrientations":
+            [
+                "UlInterfaceOrientationPortrait"
+            ]
+    ]
+    
+    static let demoInfoPlist: [String: Plist.Value] = [
+        "CFBundleShortVersionString": "\(Environment.version)",
+        "CFBundleVersion": "\(Environment.bundleVersion)",
+        "CFBundleDisplayName": "$(APP_DISPLAY_NAME)",
+        "CFBundleDevelopmentRegion": "ko",
+        "CFBundleIdentifier": "\(Environment.bundlePrefix).demo",
+        "DEFINES_MODULE": "NO",
+        "UISupportedInterfaceOrientations":
+            [
+                "UlInterfaceOrientationPortrait"
+            ]
+    ]
+}
