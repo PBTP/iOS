4100a4f1c5ce834440adbeda3e798a9415100482
bulmang <hmk113737@gmail.com>
2024-04-11 18:14:56 +0900

    [add]: create Color Extension
     Mongle에서 사용하는 Color를 상위 모듈에서 쉽게 사용할 수 있게 합니다.

---
+++ Mongle/Projects/Ui/DesignSystem/Sources/ColorExtension.swift
@@ -0,0 +1,40 @@
+//
+//  ColorExtension.swift
+//  DesignSystem
+//
+//  Created by bulmang on 4/11/24.
+//  Copyright © 2024 Mongle-iOS. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+/// Mongle이 사용하는 Color입니다.
+public extension Color {
+    
+    // PrimaryColor
+    static let mongleColorPrimary50 = Color(asset:DesignSystemAsset.mongleColorPrimary50)
+    static let mongleColorPrimary100 = Color(asset:DesignSystemAsset.mongleColorPrimary100)
+    static let mongleColorPrimary200 = Color(asset:DesignSystemAsset.mongleColorPrimary200)
+    static let mongleColorPrimary300 = Color(asset:DesignSystemAsset.mongleColorPrimary300)
+    static let mongleColorPrimary400 = Color(asset:DesignSystemAsset.mongleColorPrimary400)
+    static let mongleColorPrimary500 = Color(asset:DesignSystemAsset.mongleColorPrimary500)
+    static let mongleColorPrimary600 = Color(asset:DesignSystemAsset.mongleColorPrimary600)
+    static let mongleColorPrimary700 = Color(asset:DesignSystemAsset.mongleColorPrimary700)
+    static let mongleColorPrimary800 = Color(asset:DesignSystemAsset.mongleColorPrimary800)
+    static let mongleColorPrimary900 = Color(asset:DesignSystemAsset.mongleColorPrimary900)
+    
+    // GrayScale
+    static let mognleGrayScale0 = Color(asset:DesignSystemAsset.mongleGrayScale0)
+    static let mognleGrayScale100 = Color(asset:DesignSystemAsset.mongleGrayScale100)
+    static let mognleGrayScale200 = Color(asset:DesignSystemAsset.mongleGrayScale200)
+    static let mognleGrayScale300 = Color(asset:DesignSystemAsset.mongleGrayScale300)
+    static let mognleGrayScale400 = Color(asset:DesignSystemAsset.mongleGrayScale400)
+    static let mognleGrayScale500 = Color(asset:DesignSystemAsset.mongleGrayScale500)
+    static let mognleGrayScale600 = Color(asset:DesignSystemAsset.mongleGrayScale600)
+    static let mognleGrayScale700 = Color(asset:DesignSystemAsset.mongleGrayScale700)
+    static let mognleGrayScale800 = Color(asset:DesignSystemAsset.mongleGrayScale800)
+    static let mognleGrayScale900 = Color(asset:DesignSystemAsset.mongleGrayScale900)
+    static let mognleGrayScale1000 = Color(asset:DesignSystemAsset.mongleGrayScale1000)
+}
+
