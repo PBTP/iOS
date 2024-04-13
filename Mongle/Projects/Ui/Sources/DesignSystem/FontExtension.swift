befab288084e4fa50bc119e739970530b9b6a16d
bulmang <hmk113737@gmail.com>
2024-04-13 20:22:02 +0900

    [style]: add image assets

---
+++ Mongle/Projects/Ui/Sources/DesignSystem/FontExtension.swift
@@ -0,0 +1,31 @@
+//
+//  FontExtension.swift
+//  DesignSystem
+//
+//  Created by bulmang on 4/11/24.
+//  Copyright © 2024 Mongle-iOS. All rights reserved.
+//
+
+import SwiftUI
+
+public extension Font {
+    // Heading
+    static let mgHeading1 = UiFontFamily.Pretendard.bold.swiftUIFont(size: 24)
+    static let mgHeading2 = UiFontFamily.Pretendard.bold.swiftUIFont(size: 22)
+
+    // Title
+    static let mgTitle1 = UiFontFamily.Pretendard.semiBold.swiftUIFont(size: 17)
+    static let mgTitle2 = UiFontFamily.Pretendard.semiBold.swiftUIFont(size: 16)
+    static let mgTitle3 = UiFontFamily.Pretendard.medium.swiftUIFont(size: 15)
+
+    // Body
+    static let mgBody1 = UiFontFamily.Pretendard.medium.swiftUIFont(size: 18)
+    static let mgBody2 = UiFontFamily.Pretendard.medium.swiftUIFont(size: 16)
+    static let mgBody3 = UiFontFamily.Pretendard.medium.swiftUIFont(size: 14)
+    static let mgBody4 = UiFontFamily.Pretendard.medium.swiftUIFont(size: 13)
+    
+    // Caption
+    static let mgCaption1 = UiFontFamily.Pretendard.medium.swiftUIFont(size: 11)
+}
+
+
---
+++ Mongle/Projects/Ui/Sources/DesignSystem/ImageExtension.swift
@@ -0,0 +1,15 @@
+//
+//  ImageExtension.swift
+//  DesignSystem
+//
+//  Created by bulmang on 4/12/24.
+//  Copyright © 2024 Mongle-iOS. All rights reserved.
+//
+
+import SwiftUI
+
+public extension Image {
+    static let appleLogin = UiAsset.Images.appleLogin
+    static let kakakaoLogin = UiAsset.Images.kakaoLogin
+    static let xCloseIcon = UiAsset.Images.xCloseIcon
+}
