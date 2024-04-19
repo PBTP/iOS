0e9e0f7f65019449adce59cb55f3619ff7024fb8
bulmang <hmk113737@gmail.com>
2024-04-19 18:38:39 +0900

    [feat]: connect agree terms sheet

--- Mongle/Projects/Features/OnBoardingFeature/Sources/LoginView.swift
+++ Mongle/Projects/Features/OnBoardingFeature/Sources/LoginView.swift
@@ -6,11 +6,16 @@
 //  Copyright © 2024 Mongle-iOS. All rights reserved.
 //
 
-import Ui
+import ComposableArchitecture
 import SwiftUI
+import Ui
 
 public struct LoginView: View {
-    public init() {}
+    let store: Store<AgreeStore.State, AgreeStore.Action>
+    
+    public init(store: Store<AgreeStore.State, AgreeStore.Action>) {
+        self.store = store
+    }
     
     public var body: some View {
             VStack(spacing: 12) {
@@ -18,18 +23,30 @@ public struct LoginView: View {
                 
                 Spacer()
                 
-                KaKaoLoginButton {}
+                KaKaoLoginButton {
+                    self.store.send(.toggleChanged(isOn: true))
+                }
                 
-                AppleLoginButton {}
+                AppleLoginButton {
+                    self.store.send(.toggleChanged(isOn: true))
+                }
                 
                 SkipLoginButton {}
                 .padding(.top, 24)
                 .padding(.bottom, 50)
             }
             .padding(.horizontal, 20)
+            .sheet(isPresented: Binding(get: { self.store.state.isPressed }, set: { _ in })) {
+                NavigationView {
+                    AgreeTermsSheet(store: store)
+                }
+                .presentationDetents([.height(374)])
+            }
     }
 }
 
 #Preview {
-    LoginView()
+        LoginView(
+            store: Store(initialState: AgreeStore.State(data:  ["전체 동의","만 14세 이상 이용, 서비스 이용약관, 개인정보 수집\n 및 이용 동의 (필수)", "개인정보 수집 및 이용 동의 (선택)", "특가, 쿠폰 등 마케팅 정보 수신 동의 (선택)", "위치 정보 이용 약관 동의 (선택)"])) {AgreeStore()}
+        )
 }
