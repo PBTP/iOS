88ab1049d0d51c226a6a28e1e35d37062c1fc2e8
bulmang <hmk113737@gmail.com>
2024-04-19 16:34:56 +0900

    [design]: create telecom select button ui

---
+++ Mongle/Projects/Ui/Sources/ButtonComponent/TelecomSelectButton.swift
@@ -0,0 +1,112 @@
+//
+//  TelecomrSelectionButton.swift
+//  Ui
+//
+//  Created by bulmang on 4/16/24.
+//  Copyright © 2024 Mongle-iOS. All rights reserved.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+@Reducer
+public struct phoneNumberStore {
+    public init() {
+    
+    }
+    
+    @ObservableState
+    public struct State: Equatable {
+        public init(isPressed: Bool = false, data: [String], selectedText: String? = nil) {
+            self.isPressed = isPressed
+            self.data = data
+            self.selectedText = selectedText
+        }
+        var isPressed = false
+        var data: [String] = ["SKT", "KT", "LG U+", "SKT 알뜰폰", "KT 알뜰폰", "LG U+ 알뜰폰"]
+        var selectedText: String?
+    }
+    
+    public enum Action {
+        case toggleChanged(isOn: Bool)
+        case selectText(index: Int)
+    }
+    
+    public var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case let .toggleChanged(isOn):
+                state.isPressed = isOn
+                return .none
+            
+            case let .selectText(index):
+                state.selectedText = state.data[index]
+                return .none
+            }
+        }
+    }
+}
+
+public struct TelecomSelectButton: View {
+    let store: Store<phoneNumberStore.State, phoneNumberStore.Action>
+    let action: () -> Void
+    
+    public init(store: Store<phoneNumberStore.State, phoneNumberStore.Action>, action: @escaping () -> Void) {
+        self.store = store
+        self.action = action
+    }
+    
+    public var body: some View {
+        Button(action: {
+            print("통신사 선택 버튼 클릭")
+            self.store.send(.toggleChanged(isOn: true))
+            action()
+        }, label: {
+            Text("\(store.selectedText ?? "통신사 선택")")
+                .font(.mgBody1)
+                .foregroundColor(store.selectedText == nil ? Color.mongleGrayScale300 : Color.black)
+                .padding(.leading, 16)
+                .padding(.vertical, 12)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .background(Color.mongleGrayScale100)
+                .clipShape(RoundedRectangle(cornerRadius: 8))
+        })
+        .sheet(isPresented: Binding(get: { self.store.state.isPressed }, set: { _ in })) {
+            NavigationView {
+                List {
+                    ForEach(store.state.data, id: \.self) { text in
+                        Button(action: {
+                            // 버튼을 눌렀을 때 할 일을 여기에 구현합니다.
+                            print("\(text) 통신사 선택")
+                            if let index = store.state.data.firstIndex(of: text) {
+                                self.store.send(.selectText(index: index))
+                                self.store.send(.toggleChanged(isOn: false))
+                            }
+                        }) {
+                            Text(text)
+                        }
+                    }
+                }
+                .listStyle(.plain)
+                .navigationBarTitle(Text("통신사 선택"), displayMode: .inline)
+                .navigationBarItems(trailing: Button(action: {
+                    self.store.send(.toggleChanged(isOn: false))
+                }) {
+                    Image.xCloseIcon
+                })
+            }
+            .presentationDetents([.height(374)])
+        }
+    }
+}
+
+
+
+#Preview {
+    TelecomSelectButton(
+        store: Store(initialState: phoneNumberStore.State(data: ["test"])) {phoneNumberStore()}
+    ) {}
+}
+
+    
+
