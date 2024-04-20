0f19c7bc581655a57f8f444e012abfbbd576eeb2
bulmang <hmk113737@gmail.com>
2024-04-20 20:02:35 +0900

    [design]: create button components

--- Mongle/Projects/Ui/Sources/ButtonComponent/ConfirmButtonComponent.swift
+++ Mongle/Projects/Ui/Sources/ButtonComponent/ConfirmButtonComponent.swift
@@ -39,6 +39,41 @@ public func RequestButton(_ buttonText: String, action: @escaping () -> Void) ->
     }
 }
 
+public func mongleButton(_ buttonText: String, buttonColor: Color, textColor: Color, textFont: Font, verticalPadding: Double, horizontalPadding: Double, radius: Double, action: @escaping () -> Void) -> some View {
+    Button {
+        print("\(buttonText) 버튼 클릭")
+        action()
+    } label: {
+            Text(buttonText)
+                .font(textFont)
+                .foregroundColor(textColor)
+                .padding(.vertical, verticalPadding)
+                .padding(.horizontal, horizontalPadding)
+                .background(buttonColor)
+                .clipShape(RoundedRectangle(cornerRadius: radius))
+    }
+}
+
+public func iconHorizontalButton(_ buttonText: String, buttonColor: Color, textColor: Color, textFont: Font, verticalPadding: Double, horizontalPadding: Double, radius: Double, iconImage: Image, spacing: Double, action: @escaping () -> Void) -> some View {
+    Button {
+        print("\(buttonText) 버튼 클릭")
+        action()
+    } label: {
+            HStack(spacing: spacing) {
+                iconImage
+                
+                Text(buttonText)
+                    .font(textFont)
+                    .foregroundColor(textColor)
+            }
+            .padding(.vertical, verticalPadding)
+            .padding(.horizontal, horizontalPadding)
+            .background(buttonColor)
+            .clipShape(RoundedRectangle(cornerRadius: radius))
+    }
+}
+
+
 #Preview {
     ZStack {
         Color.mongleGrayScale200
@@ -47,6 +82,9 @@ public func RequestButton(_ buttonText: String, action: @escaping () -> Void) ->
             ConfirmButton("인증번호 전송", isDisabeld: false) {}
             
             RequestButton("재요청") {}
+            
+            iconHorizontalButton("목욕 업체 둘러보기", buttonColor: Color.mongleGrayScale500, textColor: Color.mongleGrayScale0, textFont: .mgBody4, verticalPadding: 11, horizontalPadding: 12, radius: 8, iconImage: Image.searchIcon, spacing: 8) {
+            }
         }
     }
 }
