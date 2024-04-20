69616fdb7fe357d69b0eff06b9762d35cb045071
bulmang <hmk113737@gmail.com>
2024-04-20 23:32:38 +0900

    [add]: add profile feature demo app

---
+++ Mongle/Projects/Features/ProfileFeature/Demo/Sources/ProfileFeatureDemoApp.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileFeatureDemoApp.swift
+//  ProfileFeatureDemo
+//
+//  Created by bulmang on 4/20/24.
+//  Copyright © 2024 Mongle-iOS. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct ProfileFeatureDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("ProfileFeatureDemoApp")
+        }
+    }
+}
+
