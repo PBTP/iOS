0cf28c4ccad60bd49ee290a132a463020dbc57cc
bulmang <hmk113737@gmail.com>
2024-05-01 20:20:49 +0900

    [style]: add icon image assets[style]:

--- Mongle/Projects/Ui/Sources/DesignSystem/ImageExtension.swift
+++ Mongle/Projects/Ui/Sources/DesignSystem/ImageExtension.swift
@@ -12,11 +12,13 @@ public extension Image {
     static let appleLogin = UiAsset.Images.appleLogin.swiftUIImage
     static let arrowLeftIcon = UiAsset.Images.arrowLeftIcon.swiftUIImage
     static let bellIcon = UiAsset.Images.bellIcon.swiftUIImage
+    static let calendarIcon = UiAsset.Images.calendarIcon.swiftUIImage
     static let checkBoxEmptyIcon = UiAsset.Images.checkboxEmptyIcon.swiftUIImage
     static let checkBoxFillIcon = UiAsset.Images.checkboxFillIcon.swiftUIImage
     static let chevronDownIcon = UiAsset.Images.chevronDownIcon.swiftUIImage
     static let chevronRightIcon = UiAsset.Images.chevronRightIcon.swiftUIImage
     static let gearIcon = UiAsset.Images.gearIcon.swiftUIImage
+    static let heartEmptyIcon = UiAsset.Images.heartEmptyIcon.swiftUIImage
     static let kakakaoLogin = UiAsset.Images.kakaoLogin.swiftUIImage
     static let markerPinIcon = UiAsset.Images.markerPinIcon.swiftUIImage
     static let pencilIcon = UiAsset.Images.pencilIcon.swiftUIImage
