d19c4b6d5ba5815c7c225a9c7b59d0536758ab0e
bulmang <hmk113737@gmail.com>
2024-05-01 15:54:16 +0900

    [style]: add icon image asset

--- Mongle/Projects/Ui/Sources/DesignSystem/ImageExtension.swift
+++ Mongle/Projects/Ui/Sources/DesignSystem/ImageExtension.swift
@@ -10,16 +10,19 @@ import SwiftUI
 
 public extension Image {
     static let appleLogin = UiAsset.Images.appleLogin.swiftUIImage
-    static let kakakaoLogin = UiAsset.Images.kakaoLogin.swiftUIImage
-    static let xCloseIcon = UiAsset.Images.xCloseIcon.swiftUIImage
     static let arrowLeftIcon = UiAsset.Images.arrowLeftIcon.swiftUIImage
+    static let bellIcon = UiAsset.Images.bellIcon.swiftUIImage
     static let checkBoxEmptyIcon = UiAsset.Images.checkboxEmptyIcon.swiftUIImage
     static let checkBoxFillIcon = UiAsset.Images.checkboxFillIcon.swiftUIImage
     static let chevronDownIcon = UiAsset.Images.chevronDownIcon.swiftUIImage
-    static let searchIcon = UiAsset.Images.searchIcon.swiftUIImage
     static let chevronRightIcon = UiAsset.Images.chevronRightIcon.swiftUIImage
-    static let markerPinIcon = UiAsset.Images.markerPinIcon.swiftUIImage
     static let gearIcon = UiAsset.Images.gearIcon.swiftUIImage
-    static let plucIcon = UiAsset.Images.plucIcon.swiftUIImage
+    static let kakakaoLogin = UiAsset.Images.kakaoLogin.swiftUIImage
+    static let markerPinIcon = UiAsset.Images.markerPinIcon.swiftUIImage
     static let pencilIcon = UiAsset.Images.pencilIcon.swiftUIImage
+    static let plucIcon = UiAsset.Images.plucIcon.swiftUIImage
+    static let searchGrayIcon = UiAsset.Images.searchGrayIcon.swiftUIImage
+    static let searchIcon = UiAsset.Images.searchIcon.swiftUIImage
+    static let xCloseIcon = UiAsset.Images.xCloseIcon.swiftUIImage
 }
+
