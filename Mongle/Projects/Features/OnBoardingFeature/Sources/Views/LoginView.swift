dedd0a72d8220b620ad09bef943bac3de4bc5e98
bulmang <hmk113737@gmail.com>
2024-05-15 21:00:39 +0900

    DMVM-142(feature): feat apple sign in button

--- Mongle/Projects/Features/OnBoardingFeature/Sources/Views/LoginView.swift
+++ Mongle/Projects/Features/OnBoardingFeature/Sources/Views/LoginView.swift
@@ -6,7 +6,9 @@
 //  Copyright © 2024 Mongle-iOS. All rights reserved.
 //
 
+import AuthenticationServices
 import ComposableArchitecture
+import Core
 import SwiftUI
 import Ui
 import KakaoSDKCommon
@@ -16,37 +18,72 @@ import KakaoSDKUser
 public struct LoginView: View {
     public init() {
         if let appKey = ProcessInfo.processInfo.environment["KAKAO_APP_KEY"] {
-                  KakaoSDK.initSDK(appKey: appKey)
         }
     }
     
-    let store = Store(initialState: OnBoardingReducer.State.authentication(.init())) {
+    let store = Store(initialState: OnBoardingReducer.State.kakaoAuthentication(.init())) {
         OnBoardingReducer.body._printChanges()
     }
     
     public var body: some View {
         
-            VStack(spacing: 12) {
-                HeaderComponent(headerText: "로그인", iconImageName: Image.xCloseIcon) {}
-                
-                Spacer()
-                
-                KaKaoLoginButton {
-                    store.send(.authentication(.loginKakaoAccount))
-                }
-                
-                AppleLoginButton {}
-                
-                SkipLoginButton {}
+        VStack(spacing: 12) {
+            HeaderComponent(headerText: "로그인", iconImageName: Image.xCloseIcon) {}
+            
+            Spacer()
+            
+            KaKaoLoginButton {
+                store.send(.kakaoAuthentication(.loginKakaoAccount))
+            }
+            
+            AppleSigninButton()
+            
+            SkipLoginButton {}
                 .padding(.top, 24)
                 .padding(.bottom, 50)
+        }
+        .padding(.horizontal, 20)
+        .onOpenURL(perform: { url in
+            if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                _ = AuthController.handleOpenUrl(url: url)
             }
-            .padding(.horizontal, 20)
-            .onOpenURL(perform: { url in
-                if (AuthApi.isKakaoTalkLoginUrl(url)) {
-                    _ = AuthController.handleOpenUrl(url: url)
+        })
+    }
+}
+
+struct AppleSigninButton : View {
+    let store = Store(initialState: AppleAuthCore.State()) {
+        AppleAuthCore().body._printChanges()
+    }
+    
+    var body: some View{
+        VStack {
+            SignInWithAppleButton(
+                .continue,
+                onRequest: { request in
+                    request.requestedScopes = [.fullName, .email]
+                },
+                onCompletion: { result in
+                    switch result {
+                    case .success(let authResults):
+                        print("Apple Login Successful")
+                        switch authResults.credential{
+                        case let appleIDCredential as ASAuthorizationAppleIDCredential:
+                            store.send(.login(appleIDCredential))
+                            
+                        default:
+                            break
+                        }
+                    case .failure(let error):
+                        print(error.localizedDescription)
+                        print("error")
+                    }
                 }
-            })
+            )
+            .frame(maxWidth: .infinity)
+            .frame(height:46)
+            .cornerRadius(8)
+        }
     }
 }
 
