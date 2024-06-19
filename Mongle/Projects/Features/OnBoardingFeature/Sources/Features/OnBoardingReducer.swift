ed4dc00e457e6ec727d3f3731e66fd902cdae724
bulmang <hmk113737@gmail.com>
2024-06-19 18:08:49 +0900

    DMVM-[141](remove): remove kakao reducer

--- Mongle/Projects/Features/OnBoardingFeature/Sources/Features/OnBoardingReducer.swift
+++ Mongle/Projects/Features/OnBoardingFeature/Sources/Features/OnBoardingReducer.swift
@@ -12,7 +12,6 @@ import ComposableArchitecture
 @Reducer(state: .equatable)
 public enum OnBoardingReducer {
     case agree(AgreeStore)
-    case kakaoAuthentication(KaKaoAuthCore)
     
     public static var body: some ReducerOf<Self> {
         Reduce { state, action in
@@ -20,21 +19,14 @@ public enum OnBoardingReducer {
             case .agree:
                 state = .agree(AgreeStore.State())
                 return .none
-            case .kakaoAuthentication:
-                state = .kakaoAuthentication(KaKaoAuthCore.State())
-                return .none
             }
         }
         .ifCaseLet(\.agree, action: \.agree) {
             AgreeStore()
-        }
-        .ifCaseLet(\.kakaoAuthentication, action: \.kakaoAuthentication) {
-            KaKaoAuthCore()
         }
     }
 }
 
 public enum OnBoardingAction {
     case agree(AgreeStore.Action)
-    case kakaoAuthentication(KaKaoAuthCore.Action)
 }
