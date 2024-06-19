65d19a7b9a9d6d91b7e2024c2470e6211cf3a9c9
bulmang <hmk113737@gmail.com>
2024-06-19 18:07:58 +0900

    DMVM-[141](feat): feat send apple data

--- Mongle/Projects/Core/Sources/Authentication/AppleAuthCore.swift
+++ Mongle/Projects/Core/Sources/Authentication/AppleAuthCore.swift
@@ -15,40 +15,46 @@ public struct AppleAuthCore {
     
     @ObservableState
     public struct State: Equatable {
-        public var userIdentifier: String?
-        public var fullName: String?
-        public var name: String?
-        public var email: String?
-        public var identityToken: String?
-        public var authorizationCode: String?
-
-        public init() {}
+        public var customer: Customer
+        
+        public init() {
+            self.customer = Customer()
+        }
     }
     
     public enum Action {
+        case appleButtonTapped(ASAuthorizationAppleIDCredential)
         case login(ASAuthorizationAppleIDCredential)
-        case loginInfoPrint
+        case loginError(Error)
     }
     
     public var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
             case let .login(credentials):
-                state.userIdentifier = credentials.user
-                state.fullName = (credentials.fullName?.familyName ?? "") + (credentials.fullName?.givenName ?? "")
-                state.email = credentials.email?.debugDescription ?? ""
-                state.identityToken = String(data: credentials.identityToken!, encoding: .utf8) ?? ""
-                state.authorizationCode = String(data: credentials.authorizationCode!, encoding: .utf8) ?? ""
-                print("state: \(state)")
-                    
+                state.customer.uuid = credentials.user
+                
+                if let familyName = credentials.fullName?.familyName, let givenName = credentials.fullName?.givenName {
+                    state.customer.customerName = familyName + givenName
+                } else {
+                    state.customer.customerName = nil
+                }
+                
+                state.customer.authProvider = "APPLE"
+                
+                NetworkManager.shared.sendUserData(customer: state.customer)
+                
                 return .none
                 
-            case .loginInfoPrint:
-                print("user identifier: \(String(describing: state.userIdentifier ?? nil))")
-                print("user identityToken: \(String(describing: state.identityToken ?? nil))")
-                print("user fullName: \(String(describing: state.fullName ?? nil))")
-                print("email: \(String(describing: state.email ?? nil))")
-                print("authorizationCode: \(String(describing: state.authorizationCode ?? nil))")
+            case let .appleButtonTapped(credientials):
+                return .run { send in
+                    await send(.login(credientials))
+                } catch: { error, send in
+                    await send(.loginError(error))
+                }
+                
+            case let .loginError(error):
+                print(error)
                 return .none
             }
         }
