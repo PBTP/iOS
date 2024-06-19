7641ecb98a7f2245bc7da8b089f0a544928a1cdd
bulmang <hmk113737@gmail.com>
2024-06-19 18:08:08 +0900

    DMVM-[141](feat): feat send kakao data

--- Mongle/Projects/Core/Sources/Authentication/KaKaoAuthCore.swift
+++ Mongle/Projects/Core/Sources/Authentication/KaKaoAuthCore.swift
@@ -1,139 +1,72 @@
 //
-//  AuthenticationReducer.swift
+//  kakaoAuth.swift
 //  Core
 //
-//  Created by bulmang on 5/12/24.
+//  Created by bulmang on 6/12/24.
 //  Copyright © 2024 Mongle-iOS. All rights reserved.
 //
 
-import ComposableArchitecture
-import KakaoSDKAuth
+import Foundation
 import KakaoSDKUser
-import KakaoSDKCommon
 
-@Reducer
-public struct KaKaoAuthCore {
-    public init() {}
+public class KaKaoAuthCore: ObservableObject {
+    @Published public var customer: Customer
     
-    @ObservableState
-    public struct State: Equatable {
-        public var kakaoToken: String = ""
-        
-        public init() {}
+    public init() {
+        self.customer = Customer()
     }
     
-    public enum Action {
-        case loginKakaoAccount
-        case logoutKakaoAccount
-        case hasToken
-        case getTokenInfo
-        case getUserInfo
-        case unLinkKakaoAccount
-    }
-    
-    public var body: some Reducer<State, Action> {
-        Reduce { state, action in
-            switch action {
-                
-            case .loginKakaoAccount:
-                if UserApi.isKakaoTalkLoginAvailable() {
-                    UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
-                        if let error = error {
-                            print(error)
-                        }
-                        else {
-                            print("loginWithKakaoTalk() success.")
-                            
-                            //do something
-                            _ = oauthToken
-                        }
-                    }
-                } else {
-                    UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
-                        if let error = error {
-                            print(error)
-                        }
-                        else {
-                            print("loginWithKakaoAccount() success.")
-                            
-                            //do something
-                            _ = oauthToken
-                        }
-                    }
-                }
-                return .none
-                
-            case .logoutKakaoAccount:
-                UserApi.shared.logout {(error) in
-                    if let error = error {
-                        print(error)
-                    }
-                    else {
-                        print("logout() success.")
-                    }
-                }
-                return .none
-                
-            case .hasToken:
-                if AuthApi.hasToken() {
-                    UserApi.shared.accessTokenInfo { (_, error) in
-                        if let error = error {
-                            if let sdkError = error as? SdkError, sdkError.isInvalidTokenError() == true  {
-                                print("로그인 필요")
-                            }
-                            else {
-                                print(error)
-                            }
-                        }
-                        else {
-                            print("토큰 유효성 체크 성공(필요 시 토큰 갱신됨)")
-                        }
-                    }
+    public func loginKakaoAccount() {
+        if UserApi.isKakaoTalkLoginAvailable() {
+            UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+                if let error = error {
+                    print(error)
                 }
                 else {
-                    print("로그인 필요")
+                    print("loginWithKakaoTalk() success.")
+                    
+                    self.getUserInfo()
                 }
-                return .none
+            }
+        } else {
+            UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+                if let error = error {
+                    print(error)
+                }
+                else {
+                    print("loginWithKakaoAccount() success.")
+                    
+                    self.getUserInfo()
+                }
+            }
+        }
+    }
+    
+    private func getUserInfo() {
+        UserApi.shared.me() { [weak self] (user, error) in
+            if let error = error {
+                print(error)
+            } else {
+                print("me() success.")
+                guard let self = self else { return }
                 
-            case .getTokenInfo:
-                print("getTokenInfo() called")
-                UserApi.shared.accessTokenInfo {(accessTokenInfo, error) in
-                    if let error = error {
-                        print(error)
-                    }
-                    else {
-                        print("accessTokenInfo() success.")
-                        
-                        //do something
-                        _ = accessTokenInfo
-                    }
+                if let uuid = user?.id?.description {
+                    self.customer.uuid = uuid
                 }
-                return .none
                 
-            case .getUserInfo:
-                UserApi.shared.me() {(user, error) in
-                    if let error = error {
-                        print(error)
-                    }
-                    else {
-                        print("me() success.")
-                        
-                        //do something
-                        _ = user
-                    }
+                if let customerName = user?.kakaoAccount?.profile?.nickname {
+                    self.customer.customerName = customerName
                 }
-                return .none
                 
-            case .unLinkKakaoAccount:
-                UserApi.shared.unlink {(error) in
-                    if let error = error {
-                        print(error)
-                    }
-                    else {
-                        print("unlink() success.")
-                    }
+                if let customerPhoneNumber = user?.kakaoAccount?.phoneNumber {
+                    self.customer.customerPhoneNumber = customerPhoneNumber
                 }
-                return .none
+                
+                self.customer.authProvider = "KAKAO"
+                
+                print(self.customer)
+                
+                NetworkManager.shared.sendUserData(customer: customer)
             }
         }
     }
