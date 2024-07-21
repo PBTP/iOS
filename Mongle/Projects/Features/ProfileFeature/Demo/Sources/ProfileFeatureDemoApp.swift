d018fb33f4f87f36190c93fcdec4d59c9364ca7d
bulmang <hmk113737@gmail.com>
2024-07-21 22:16:53 +0900

    DMVM-[156](chore): profile demo app build configuration

--- Mongle/Projects/Features/ProfileFeature/Demo/Sources/ProfileFeatureDemoApp.swift
+++ Mongle/Projects/Features/ProfileFeature/Demo/Sources/ProfileFeatureDemoApp.swift
@@ -6,14 +6,18 @@
 //  Copyright © 2024 Mongle-iOS. All rights reserved.
 //
 
+import Core
 import SwiftUI
 import ProfileFeature
 
 @main
 struct ProfileFeatureDemoApp: App {
+    @StateObject var kakaoAuth = KaKaoAuthCore()
+    
     var body: some Scene {
         WindowGroup {
             ProfileView()
+                .environmentObject(kakaoAuth)
         }
     }
 }
