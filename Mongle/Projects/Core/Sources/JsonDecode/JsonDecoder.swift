767840b4dcb3cc872a4ee8313d1a5404194bccf8
bulmang <hmk113737@gmail.com>
2024-08-20 21:15:23 +0900

    DMVM-[156](feat): make class public and add formatAddress function

--- Mongle/Projects/Core/Sources/JsonDecode/JsonDecoder.swift
+++ Mongle/Projects/Core/Sources/JsonDecode/JsonDecoder.swift
@@ -8,10 +8,9 @@
 
 import Foundation
 
-class JsonDecoder {
-    static let shared = JsonDecoder()
+public class JsonDecoder {
     
-    private init() {}
+    public init() {}
     
     func decodeCustomer(from jsonString: String) -> Customer? {
         let jsonData = jsonString.data(using: .utf8)!
@@ -22,6 +21,18 @@ class JsonDecoder {
         } catch {
             print("Error decoding JSON: \(error)")
             return nil
+        }
+    }
+    
+    public func formatAddress(from jsonString: String) -> String {
+        let jsonData = Data(jsonString.utf8)
+        
+        do {
+            let address = try JSONDecoder().decode(Address.self, from: jsonData)
+            return "\(address.address) \(address.detailAddress)"
+        } catch {
+            print("Failed to decode JSON: \(error)")
+            return "Error decoding JSON"
         }
     }
 }
