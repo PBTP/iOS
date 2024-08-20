5a2efa7e867e52557796e203b9e79b6e6f230083
bulmang <hmk113737@gmail.com>
2024-08-20 21:13:52 +0900

    DMVM-[156](feat): create pet struct

---
+++ Mongle/Projects/Core/Sources/Model/Pet.swift
@@ -0,0 +1,20 @@
+//
+//  Pet.swift
+//  Core
+//
+//  Created by bulmang on 7/31/24.
+//  Copyright © 2024 Mongle-iOS. All rights reserved.
+//
+
+import Foundation
+
+public struct Pet {
+    var name: String
+    var gender: String
+    var breed: String
+    var birthDate: Date
+    var isNeutered: Bool
+    var weight: Int
+    var temperament: String
+}
+
