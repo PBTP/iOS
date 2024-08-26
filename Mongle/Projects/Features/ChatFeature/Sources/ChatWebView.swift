63d384075f1029960cd561c1d9c8fc2654e64b92
bulmang <hmk113737@gmail.com>
2024-08-26 18:09:27 +0900

    DMVM-197(Refactor): reflecting code review feedback
    변수 및 구조체 이름 변경, 가독성 반영

--- Mongle/Projects/Features/ChatFeature/Sources/ChatWebView.swift
+++ Mongle/Projects/Features/ChatFeature/Sources/ChatWebView.swift
@@ -14,20 +14,21 @@ import WebKit
 struct ChatWebView: View {
     @EnvironmentObject var kakaoAuth: KaKaoAuthCore
     
-    @State private var webUiView: ChatWebUiView?
+    @State private var webUIView: ChatWebUIView?
     
     var body: some View {
         VStack {
-            if let webUiView = webUiView {
+            if let webUiView = webUIView {
                 webUiView
                     .ignoresSafeArea()
             } else {
+                // TODO: 오류 표시 페이지 추가
                 ProgressView()
             }
         }
         .onAppear {
             let request = URLRequest(url: URL(string: Web.url(for: "chat-list"))!)
-            self.webUiView = ChatWebUiView(request: request, kakaoAuthCore: kakaoAuth)
+            self.webUIView = ChatWebUIView(request: request, kakaoAuthCore: kakaoAuth)
         }
     }
 }
@@ -42,7 +43,8 @@ class ContentController: NSObject, WKScriptMessageHandler, WKNavigationDelegate 
     }
 
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        guard let token = kakaoAuthCore.customer.accessToken, let uuid = kakaoAuthCore.customer.uuid else {
+        guard let token = kakaoAuthCore.customer.accessToken, 
+                let uuid = kakaoAuthCore.customer.uuid else {
             return
         }
 
@@ -61,20 +63,23 @@ class ContentController: NSObject, WKScriptMessageHandler, WKNavigationDelegate 
     }
 }
 
-struct ChatWebUiView: UIViewRepresentable {
+struct ChatWebUIView: UIViewRepresentable {
     let request: URLRequest
     private var webView: WKWebView
     private var contentController: ContentController
 
     init(request: URLRequest, kakaoAuthCore: KaKaoAuthCore) {
-        let contentController = ContentController(kakaoAuthCore: kakaoAuthCore)
+        self.contentController = ContentController(kakaoAuthCore: kakaoAuthCore)
+        self.request = request
+        self.webView = ChatWebUIView.createWebView(contentController)
+    }
+    
+    private static func createWebView(_ contentController: ContentController) -> WKWebView {
         let config = WKWebViewConfiguration()
         config.userContentController.add(contentController, name: "handleIosWebviewToken")
-
-        self.webView = WKWebView(frame: .zero, configuration: config)
-        self.request = request
-        self.contentController = contentController
-        self.webView.navigationDelegate = contentController
+        let webView = WKWebView(frame: .zero, configuration: config)
+        webView.navigationDelegate = contentController
+        return webView
     }
 
     func makeUIView(context: Context) -> WKWebView {
