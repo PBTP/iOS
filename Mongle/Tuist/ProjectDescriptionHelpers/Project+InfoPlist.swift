cfa005d72e1840fa0f876032a6769b45fde7dc79
보경 <qkrqhru@knu.ac.kr>
2024-09-07 00:56:06 +0900

    DMVM-204(refactor): Plist 생성 함수 리팩토링

--- Mongle/Tuist/ProjectDescriptionHelpers/Project+InfoPlist.swift
+++ Mongle/Tuist/ProjectDescriptionHelpers/Project+InfoPlist.swift
@@ -8,59 +8,35 @@
 import ProjectDescription
 
 public extension Project {
-    static let appInfoPlist: [String: Plist.Value] = [
-        "CFBundleShortVersionString": "\(Environment.version)",
-        "CFBundleVersion": "\(Environment.bundleVersion)",
-        "CFBundleDisplayName": "$(APP_DISPLAY_NAME)",
-        "CFBundleDevelopmentRegion": "ko",
-        "CFBundleIdentifier": "\(Environment.bundlePrefix).release",
-        "DEFINES_MODULE": "NO",
-        "LSApplicationQueriesSchemes" : [
-            "kakaokompassauth",
-            "kakaolink"
-        ],
-        "CFBundleURLTypes": [
-            [
-                "CFBundleTypeRole": "Editor",
-                "CFBundleURLSchemes": ["kakao\(APIKey.kakaoAppKey)"]
-            ]
-        ],
-        "UILaunchStoryboardName": "LaunchScreen",
-        "Launch Screen" : [
-            "Image Name" : "logo",
-            "Background color" : "splash_color"
-        ],
-        "UISupportedInterfaceOrientations":
-            [
+    static func makeInfoPlist(bundleIdentifierSuffix: String) -> [String: Plist.Value] {
+        return [
+            "CFBundleShortVersionString": "\(Environment.version)",
+            "CFBundleVersion": "\(Environment.bundleVersion)",
+            "CFBundleDisplayName": "$(APP_DISPLAY_NAME)",
+            "CFBundleDevelopmentRegion": "ko",
+            "CFBundleIdentifier": "\(Environment.bundlePrefix).\(bundleIdentifierSuffix)",
+            "DEFINES_MODULE": "NO",
+            "LSApplicationQueriesSchemes" : [
+                "kakaokompassauth",
+                "kakaolink"
+            ],
+            "CFBundleURLTypes": [
+                [
+                    "CFBundleTypeRole": "Editor",
+                    "CFBundleURLSchemes": ["kakao\(APIKey.kakaoAppKey)"]
+                ]
+            ],
+            "UILaunchStoryboardName": "LaunchScreen",
+            "UILaunchScreen" : [
+                "UIImageName" : "logo",
+                "UIColorName" : "splash_color"
+            ],
+            "UISupportedInterfaceOrientations": [
                 "UlInterfaceOrientationPortrait"
             ]
-    ]
-    
-    static let demoInfoPlist: [String: Plist.Value] = [
-        "CFBundleShortVersionString": "\(Environment.version)",
-        "CFBundleVersion": "\(Environment.bundleVersion)",
-        "CFBundleDisplayName": "$(APP_DISPLAY_NAME)",
-        "CFBundleDevelopmentRegion": "ko",
-        "CFBundleIdentifier": "\(Environment.bundlePrefix).demo",
-        "DEFINES_MODULE": "NO",
-        "LSApplicationQueriesSchemes" : [
-            "kakaokompassauth",
-            "kakaolink"
-        ],
-        "CFBundleURLTypes": [
-            [
-                "CFBundleTypeRole": "Editor",
-                "CFBundleURLSchemes": ["kakao\(APIKey.kakaoAppKey)"]
-            ]
-        ],
-        "UILaunchStoryboardName": "LaunchScreen",
-        "UILaunchScreen" : [
-            "UIImageName" : "logo",
-            "UIColorName" : "splash_color"
-        ],
-        "UISupportedInterfaceOrientations":
-            [
-                "UlInterfaceOrientationPortrait"
-            ]
-    ]
+        ]
+    }
+
+    static let appInfoPlist = makeInfoPlist(bundleIdentifierSuffix: "release")
+    static let demoInfoPlist = makeInfoPlist(bundleIdentifierSuffix: "demo")
 }
