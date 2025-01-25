557e795154315d22e9258c6e1e31284e9967b6e7
보경 <qkrqhru@knu.ac.kr>
2025-01-25 17:58:34 +0900

    DMVM-228(chore): 로그인 화면 수정

---
+++ Mongle/Projects/Ui/Resources/Texts/LoginText.swift
@@ -0,0 +1,16 @@
+//
+//  LoginText.swift
+//  UI
+//
+//  Created by Bokyung on 1/25/25.
+//  Copyright © 2025 Mongle. All rights reserved.
+//
+
+import Foundation
+
+public enum LoginText {
+    public static let welcomeText = "반가워요!"
+    public static let infoText = "행복한 대형견 목욕의 시작, 몽글입니다"
+    public static let browseText = "둘러보기"
+    public static let contactText = "문의하기"
+}
